473104ff2653f59dd3c9841750546f2949169e24
Andrew McDougal <andrew@ontrendwebsites.com>
2019-07-27 11:08:41 +1000

    Shop and header development

---
+++ templates/page-home.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ Template Name: Home page
+*/
+?>
+
+<?php get_header(); ?>
+
+			<div id="content">
+
+				<div id="inner-content" class="wrap cf">
+
+						<main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+
+							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+
+								<section class="entry-content cf" itemprop="articleBody">
+									<h1 itemprop="headline"><?php the_title(); ?></h1>
+
+<?php if ( has_post_thumbnail() ) : ?>
+        <?php the_post_thumbnail('full'); ?>
+    </a>
+<?php endif; ?>
+
+									<?php
+										// the content (pretty self explanatory huh)
+										the_content();
+									?>
+								</section> <?php // end article section ?>
+
+								<footer class="article-footer cf">
+
+								</footer>
+
+							</article>
+
+							<?php endwhile; endif; ?>
+
+						</main>
+
+						<?php get_sidebar(); ?>
+
+				</div>
+
+			</div>
+
+<?php get_footer(); ?>
