0d04fc617d2fdac1592fa4f22540993b19726b1b
Andrew McDougal <andrew@ontrendwebsites.com>
2019-08-18 17:48:54 +1000

    Nav and shop styles

--- functions.php
+++ functions.php
@@ -251,7 +251,7 @@ add_action('wp_enqueue_scripts', 'bones_fonts');
 function prefix_add_footer_styles() {
   wp_enqueue_script( 'custom-js', get_stylesheet_directory_uri() . '/library/js/ontrend.js?v=1' );
   wp_enqueue_style( 'hamburger-css', get_stylesheet_directory_uri() . '/library/css/hamburgers/hamburgers.css' );
-  wp_enqueue_style( 'ontrend-css', get_stylesheet_directory_uri() . '/library/css/ontrend.css?v=4' );
+  wp_enqueue_style( 'ontrend-css', get_stylesheet_directory_uri() . '/library/css/ontrend.css?v=6' );
 };
 add_action( 'get_footer', 'prefix_add_footer_styles' );
 
